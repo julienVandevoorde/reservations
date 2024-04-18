83b94c42f185164a87b4b18cd4542bb57d65d644
BilelMahli <bilel.mahli@hotmail.com>
2024-04-18 22:36:00 +0200

    Form update Artist

--- app/Http/Controllers/ArtistController.php
+++ app/Http/Controllers/ArtistController.php
@@ -50,18 +50,38 @@ class ArtistController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit($id)
     {
-        //
+        $artist = Artist::find($id);
+        
+        return view('artist.edit',[
+            'artist' => $artist,
+        ]);
     }
+
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+	   //Validation des données du formulaire
+        $validated = $request->validate([
+            'firstname' => 'required|max:60',
+            'lastname' => 'required|max:60',
+        ]);
+
+	   //Le formulaire a été validé, nous récupérons l’artiste à modifier
+        $artist = Artist::find($id);
+
+	   //Mise à jour des données modifiées et sauvegarde dans la base de données
+        $artist->update($validated);
+
+        return view('artist.show',[
+            'artist' => $artist,
+        ]);
     }
+
 
     /**
      * Remove the specified resource from storage.
